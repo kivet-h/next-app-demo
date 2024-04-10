890fa3c2e67883fd5838ad84614fd7ed8cfdf395
kivet <wanghuixiang@druid.tech>
2024-04-10 18:12:59 +0800

    feat: 使用src的目录管理源代码

--- src/pages/about-page/index.tsx
+++ src/pages/about-page/index.tsx
@@ -6,7 +6,7 @@ interface IProps {}
 const AboutPage: FC<IProps> = (props) => {
   return (
     <div className="flex items-center">
-      <div className="flex-1">AboutPage</div>
+      <div className="flex-1">AboutPage in src</div>
       <div className="flex-1 cursor-pointer text-[#000000]" onClick={() => Router.push("/posts")}>
         AboutPage to posts page
       </div>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,7 +4,7 @@ import Router from "next/router";
 interface IProps {}
 
 const Posts: FC<IProps> = (props) => {
-  return <div onClick={() => Router.push("/posts/1")}>Posts</div>;
+  return <div onClick={() => Router.push("/posts/1")}>Posts in src</div>;
 };
 
 export default Posts;
