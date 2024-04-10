7ede7e3647bf3e48ffa3808b3cc087b24d8b098f
kivet <wanghuixiang@druid.tech>
2024-04-10 11:42:14 +0800

    feat: 全局样式、Tailwind CSS

--- pages/about-page/index.tsx
+++ pages/about-page/index.tsx
@@ -4,7 +4,14 @@ import Router from "next/router";
 interface IProps {}
 
 const AboutPage: FC<IProps> = (props) => {
-  return <div onClick={() => Router.push("/posts")}>AboutPage to posts page</div>;
+  return (
+    <div className="flex items-center">
+      <div className="flex-1">AboutPage</div>
+      <div className="flex-1 cursor-pointer text-[#000000]" onClick={() => Router.push("/posts")}>
+        AboutPage to posts page
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
